1f2c41e1e0f342a35a8ca67de91c8a39047c04ff
yefangyi <314261471@qq.com>
2018-01-19 17:00:53 +0800

    spring-clound-config fix

--- moose-config/moose-config-client/src/main/java/com/moose/config/client/ConfigClientApplication.java
+++ moose-config/moose-config-client/src/main/java/com/moose/config/client/ConfigClientApplication.java
@@ -1,22 +1,10 @@
 package com.moose.config.client;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ConfigClientApplication {
-
-    @Value("${test}")
-    private String test;
-
-    @RequestMapping("/index")
-    public String home() {
-        return test;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
---
+++ moose-config/moose-config-client/src/main/java/com/moose/config/client/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.moose.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+
+    @Value("${test}")
+    private String test;
+
+    @RequestMapping("/index")
+    public String home() {
+        return test;
+    }
+
+
+}
