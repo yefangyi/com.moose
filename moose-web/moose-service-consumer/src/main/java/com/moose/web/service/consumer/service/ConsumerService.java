0de9d6d2ed6481c622834d831e1bd9b07a03ef86
yefangyi <314261471@qq.com>
2018-01-29 16:28:56 +0800

    ribbon 升级成feign

--- moose-web/moose-service-consumer/src/main/java/com/moose/web/service/consumer/service/ConsumerService.java
+++ moose-web/moose-service-consumer/src/main/java/com/moose/web/service/consumer/service/ConsumerService.java
@@ -1,8 +1,14 @@
 package com.moose.web.service.consumer.service;
 
 import com.moose.web.service.consumer.entity.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient("moose-service-user")
 public interface ConsumerService {
 
-    User findById(Long userId);
+    @GetMapping("/user/{userId}")
+    User findById(@PathVariable("userId") Long userId);
+
 }
