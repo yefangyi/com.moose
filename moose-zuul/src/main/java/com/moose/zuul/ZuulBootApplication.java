bd426d61fb62ea77c536b1127ad7d076962f6351
yefangyi <314261471@qq.com>
2018-01-31 15:44:16 +0800

    新增zuul 网关

---
+++ moose-zuul/src/main/java/com/moose/zuul/ZuulBootApplication.java
@@ -0,0 +1,17 @@
+package com.moose.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulBootApplication.class, args);
+    }
+
+}
